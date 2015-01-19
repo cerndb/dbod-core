0bc78d582fbcee982f7e33c8ff046c049f10d3c1
icoteril <ignacio.coterillo@cern.ch>
2015-01-19 14:39:08 +0100

    Passing entity name as parameter with MooseX::Getopt

--- lib/DBOD/Job.pm
+++ lib/DBOD/Job.pm
@@ -1,33 +1,23 @@
 package DBOD::Job;
+
 use Moose;
 with 'MooseX::Log::Log4perl';
 with 'MooseX::Getopt';
 
 # Check https://metacpan.org/pod/MooseX::Getopt
 
-has 'input' => (
-    is => 'rw',
-    isa => 'Str',
-    );
+# Input
+has 'entity' => ( is => 'ro', isa => 'Str', required => 1);
 
-has 'output' => (
-    is => 'rw',
-    isa => 'Str',
-    );
+# output
+has '_output' => ( is => 'rw', isa => 'Str', );
+has '_result' => ( is => 'rw', isa => 'Num', );
 
-has 'result' => (
-    is => 'rw',
-    isa => 'Num',
-);
-
-sub help {
-
-}
 
 sub run {
     my $self = shift;
-    $self->output("ran with (" . $self->input() . ") as parameters");
-    $self->log->debug( "ran with (" . $self->input() . ") as parameters");
+    $self->output("ran with (" . $self->entity() . ") as parameters");
+    $self->log->debug( "ran with (" . $self->entity() . ") as parameters");
     $self->result(0);
 }
 
--- t/job.t
+++ t/job.t
@@ -9,9 +9,10 @@ use DBOD::Job;
 
 use Data::Dumper;
 
+# Initiates logger
 BEGIN { Log::Log4perl->easy_init() };
 
-my $job = DBOD::Job->new( input => "<job input>" );
+my $job = DBOD::Job->new_with_options();
 $job->log->info("preparing to run job");
 $job->run();
 
