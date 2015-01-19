e3b664f744c78a09dad33849447275bc3ab0a4fa
icoteril <ignacio.coterillo@cern.ch>
2015-01-19 17:39:19 +0100

    Passing -ForceArray to Config::General->new() to allow 1 element array definition

--- lib/DBOD/Config.pm
+++ lib/DBOD/Config.pm
@@ -18,7 +18,8 @@ $VERSION     = 0.1;
 my $share_dir = File::ShareDir::dist_dir('DBOD');
 my $filename = LoadFile( "$share_dir/configpath.conf" );
 
-my $config_file = Config::General->new( $filename->{'PATH'} );
+my $config_file = Config::General->new( -ConfigFile => $filename->{'PATH'}, 
+                                        -ForceArray => 1); # Allows 1 element ARR
 %cfg = $config_file->getall();
 $config = \%cfg;
 
