a644fe92aee7bed963923203ed1de38e564e5121
icoteril <ignacio.coterillo@cern.ch>
2015-04-13 17:24:40 +0200

    DBOD::Config refactored to expose a configuration load method

--- lib/DBOD/Config.pm
+++ lib/DBOD/Config.pm
@@ -7,41 +7,24 @@ use Exporter;
 use Config::General;
 use YAML::Syck;
 use File::ShareDir;
-use Log::Log4perl;
-use JSON;
 
-our ($VERSION, @ISA, @EXPORT_OK, $config, $metadata, $logger_cfg);
+our ($VERSION, @ISA, @EXPORT_OK);
 
 $VERSION     = 0.1;
 @ISA         = qw(Exporter);
-@EXPORT_OK   = qw( $config %cfg $metadata);
+@EXPORT_OK   = qw( load );
 
-my $share_dir = File::ShareDir::dist_dir('DBOD');
-my $filename = LoadFile( "$share_dir/configpath.conf" );
 
-my $config_file = Config::General->new( 
-    -ConfigFile => "${share_dir}/dbod-core.conf-template", 
-    -ForceArray => 1); # Allows 1 element ARR
+sub load {
+    my $share_dir = File::ShareDir::dist_dir('DBOD');
+    my $filename = LoadFile( "$share_dir/configpath.conf" );
 
-sub _load_cache {
-    my $filename = shift;
-    my $json_text = do { 
-        local $/ = undef;
-        open(my $json_fh, "<:encoding(UTF-8)", $filename)
-            or return ();
-        <$json_fh>
-    };
-    my $nested_array = decode_json $json_text;
-    my @flat_array = map{@$_} @$nested_array;
+    my $config_file = Config::General->new( 
+        -ConfigFile => "${share_dir}/dbod-core.conf-template", 
+        -ForceArray => 1); # Allows 1 element ARR
 
-    return @flat_array;
+    my %cfg = $config_file->getall();
+    return \%cfg; 
 }
-# Reads configuration to make it available
-my %cfg = $config_file->getall();
-$config = \%cfg;
-
-# Loads entity/host entities metadata
-my %hcache = _load_cache($config->{'api'}->{'cachefile'});
-$metadata = \%hcache;
 
 1;
