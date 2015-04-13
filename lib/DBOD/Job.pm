6d8fe0e7ca6506ffd4e50b8da35f5e3f89f40bb8
icoteril <ignacio.coterillo@cern.ch>
2015-04-13 16:51:27 +0200

    DBOD::Job contains the metadata hash as an attribute

--- lib/DBOD/Job.pm
+++ lib/DBOD/Job.pm
@@ -6,21 +6,29 @@ with 'MooseX::Getopt';
 
 use Data::Dumper;
 use DBOD::Config qw( $config );
+use DBOD::Api;
 
 # Input
 has 'entity' => ( is => 'ro', isa => 'Str', required => 1);
+
+# Variables
+has 'metadata' => (is => 'rw', isa => 'HashRef');
+has 'config' => (is => 'rw', isa => 'HashRef');
 
 # output
 has '_output' => ( is => 'rw', isa => 'Str', );
 has '_result' => ( is => 'rw', isa => 'Num', );
 
-sub run {
+after 'new_with_options' => sub {
     my $self = shift;
-    $self->_output("ran with (" . $self->entity() . ") as parameters");
-    $self->log->debug( "ran with (" . $self->entity() . ") as parameters");
-    $self->log->debug( "ran with config:\n" . Dumper $config  );
-    $self->log->debug( "Parameter access: " . Dumper $config->{'parameter'}  );
-    $self->_result(0);
+};
+
+sub run {
+    my ($self, $body, $params) = @_;
+    $self->metadata(DBOD::Api::get_entity_metadata($self->entity));
+    $self->log->debug(Dumper $self->metadata);
+    my $result = $body->($self->entity, $params);
+    $self->_result($result);
 }
 
 
