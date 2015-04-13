5c819ac59b0f4558da7a056941b7407905262911
icoteril <ignacio.coterillo@cern.ch>
2015-04-13 17:27:19 +0200

    The DBOD::Job contains the configuration and metadata as attributes (avoiding the use of global variables)

--- lib/DBOD/Job.pm
+++ lib/DBOD/Job.pm
@@ -5,15 +5,16 @@ with 'MooseX::Log::Log4perl';
 with 'MooseX::Getopt';
 
 use Data::Dumper;
-use DBOD::Config qw( $config );
-use DBOD::Api;
+use DBOD::Config;
+use DBOD::Api qw( load_cache get_entity_metadata );
 
 # Input
 has 'entity' => ( is => 'ro', isa => 'Str', required => 1);
 
 # Variables
-has 'metadata' => (is => 'rw', isa => 'HashRef');
+has 'md_cache' => (is => 'rw', isa =>'HashRef');
 has 'config' => (is => 'rw', isa => 'HashRef');
+has 'metadata' => (is => 'rw', isa => 'HashRef');
 
 # output
 has '_output' => ( is => 'rw', isa => 'Str', );
@@ -25,7 +26,11 @@ after 'new_with_options' => sub {
 
 sub run {
     my ($self, $body, $params) = @_;
-    $self->metadata(DBOD::Api::get_entity_metadata($self->entity));
+    $self->config(DBOD::Config::load());
+    my %cache = load_cache($self->config);
+    $self->md_cache(\%cache);
+    $self->metadata(
+        get_entity_metadata($self->entity, $self->md_cache, $self->config));
     $self->log->debug(Dumper $self->metadata);
     my $result = $body->($self->entity, $params);
     $self->_result($result);
