3ef1547585e0cb8d76d49c7b93304c84a455a0fe
ruben <rgaspar@cern.ch>
2015-06-26 17:25:20 +0200

    New changes needed for mysql_restore

---
+++ src/blib/lib/DBOD/DB.pm
@@ -0,0 +1,89 @@
+# Copyright (C) 2015, CERN
+# This software is distributed under the terms of the GNU General Public
+# Licence version 3 (GPL Version 3), copied verbatim in the file "LICENSE".
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+package DBOD::DB;
+
+use strict;
+use warnings;
+
+use Moose;
+with 'MooseX::Log::Log4perl';
+with 'MooseX::Role::DBIx::Connector' => {
+    connection_name => 'db',
+};
+
+use Data::Dumper;
+use Try::Tiny;
+
+sub execute_sql_file {
+    my ($self, $filename) = @_;
+    try{
+        open my $fh, '<', $filename or do {
+            $self->log->error("Can't open SQL File for reading: $!");
+            return;
+            };
+        try {
+            # local $/=';';
+            my @statements = <$fh>;
+            foreach my $statement (@statements) {
+                $self->log->debug("Executing: ${statement}");
+                $self->db_conn->dbh->do($statement);
+            }
+            return 0;
+        } catch {
+            $self->log->error(
+                sprintf("An error ocurred executing SQL file:\n%s:%s", 
+                    $self->db_conn->dbh->err,
+                    $self->db_conn->dbh->errstr));
+            return $self->db_conn->dbh->err;
+        };
+    }
+    catch {
+        $self->log->error("An error ocurred reading $filename: $!");
+        return;
+    }
+}
+
+sub select {
+    my ($self, $statement, $bind_values) = @_;
+    $self->log->debug("Running SQL statement: " . $statement);
+    try {
+        if (defined $bind_values) {
+            return $self->db_conn->dbh->selectall_arrayref($statement, @{$bind_values});
+            }
+        else {
+            return $self->db_conn->dbh->selectall_arrayref($statement);
+        }
+    } catch {
+        $self->log->error(
+            sprintf("An error ocurred executing SQL statement:\n%s:%s", 
+                $self->db_conn->dbh->err,
+                $self->db_conn->dbh->errstr));
+        return $self->db_conn->dbh->err;
+    };
+}
+
+sub do {
+    my ($self, $statement, $bind_values) = @_;
+    $self->log->debug("Running SQL statement: " . $statement);
+    try {
+        if (defined $bind_values) {
+            return $self->db_conn->dbh->do($statement, undef, @{$bind_values});
+            }
+        else {
+            return $self->db_conn->dbh->do($statement,);
+        }
+    } catch {
+        $self->log->error(
+            sprintf("An error ocurred executing SQL sttatement:\n%s:%s", 
+                $self->db_conn->dbh->err,
+                $self->db_conn->dbh->errstr));
+        return $self->db_conn->dbh->err;
+    };
+}
+
+1;
---
+++ src/blib/lib/DBOD/Job.pm
@@ -0,0 +1,103 @@
+# Copyright (C) 2015, CERN
+# This software is distributed under the terms of the GNU General Public
+# Licence version 3 (GPL Version 3), copied verbatim in the file "LICENSE".
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+package DBOD::Job;
+
+use strict;
+use warnings;
+
+use Moose;
+with 'MooseX::Log::Log4perl';
+with 'MooseX::Getopt';
+
+use Data::Dumper;
+use Switch;
+use DBD::Oracle qw(:ora_session_modes :ora_types);
+
+use DBOD::Config;
+use DBOD::Api qw( load_cache get_entity_metadata );
+use DBOD::DB;
+
+
+# Input
+has 'entity' => ( is => 'ro', isa => 'Str', required => 1);
+
+# Internal attributes
+has 'md_cache' => (is => 'rw', isa =>'HashRef');
+has 'config' => (is => 'rw', isa => 'HashRef');
+has 'metadata' => (is => 'rw', isa => 'HashRef');
+has 'db' => (is => 'rw', isa => 'Object');
+
+
+# output
+has '_output' => ( is => 'rw', isa => 'Str', );
+has '_result' => ( is => 'rw', isa => 'Num', );
+
+
+sub BUILD {
+    my $self = shift;
+    # Load General Configuration from file
+    $self->config(DBOD::Config::load());
+    # Load cache file
+    my %cache = load_cache($self->config);
+    $self->md_cache(\%cache);
+    # Load entity metadata
+    $self->metadata(
+        get_entity_metadata($self->entity, $self->md_cache, $self->config));
+    # Set up db connector
+    my $db_type = lc $self->metadata->{'subcategory'};
+    my $db_user = $self->config->{$db_type}->{'db_user'};
+    my $db_password = $self->config->{$db_type}->{'db_password'};
+    my $dsn;
+    my $db_attrs;
+    switch($db_type) {
+        case 'mysql' {
+            $dsn = "DBI:mysql:host=" . $self->metadata->{'hosts'}[0] .
+             ";port=" . $self->metadata->{'port'};
+            $db_attrs = {
+                AutoCommit => 1, 
+                };
+        }
+        case 'pgsql' {
+            $dsn = "DBI:Pg:host=" . $self->metadata->{'hosts'}[0] .
+             ";port=" . $self->metadata->{'port'};
+            $db_attrs = {
+                AutoCommit => 1, 
+                };
+        }
+        case 'oracle' {
+            my $sid = $self->metadata->{'sid'};
+            $dsn = "DBI:oracle:$sid";
+            $db_attrs = {
+                AutoCommit => 1, 
+                ora_session_mode => ORA_SYSDBA,
+                ora_client_info => 'DBOD-core', 
+                ora_verbose => 0 };
+            }
+    };
+
+    $self->db(DBOD::DB->new(
+                  db_dsn  => $dsn,
+                  db_user => $db_user,
+                  db_password => $db_password,
+                  db_attrs => $db_attrs,));
+
+};
+
+sub run {
+    my ($self, $body, $params) = @_;
+    my $result = $body->($params);
+    $self->_result($result);
+}
+
+after 'run' => sub {
+    my $self = shift;
+    $self->log->info('[' . $self->_result()  . ']');
+};
+
+
+1;
