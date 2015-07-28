35a655e523ef9ee10deeb58071ccbc908d56598c
icoteril <icot@users.noreply.github.com>
2015-07-28 16:08:22 +0200

    IPALIAS: Working functionality untill DNS entry generation

--- src/lib/DBOD/IPalias.pm
+++ src/lib/DBOD/IPalias.pm
@@ -9,8 +9,10 @@ package DBOD::IPalias;
 use strict;
 use warnings;
 
+use Log::Log4perl qw(:easy);
 use DBOD::Runtime;
 use DBOD::Api;
+use DBOD::Network;
 
 sub add_alias {
     # Registers ip alias for the entity
@@ -21,28 +23,39 @@ sub add_alias {
     # Returns false if it fails, true if it succeeds
 
     my ($entity, $config) = @_;
-    DBOD::Api::set_ip_alias($entity, $config);
+    my $ipalias = "dbod-" . $entity;
+    $ipalias =~ s/\_/\-/g; # Substitutes underscores for dashes for ip-alias
+    DBOD::Api::set_ip_alias($entity, $ipalias, $config);
     my $result = DBOD::Api::get_ip_alias($entity, $config);
+    my ($dnsname, $host);
     if (defined $result) {
+        ($dnsname, $ipalias) = @{$result->{'response'}};
         # Extract dnsname and alias from JSON result of Api call
-        my ($dnsname, $ipalias) = @{$result->{'response'}};
         # Register ip alias to dns name on the CERN Network service
-        DBOD::Network::add_ip_alias($dnsname, $ipalias);
+        DBOD::Network::add_ip_alias($dnsname, $ipalias, $config);
         # Generates DNS entry
         my $cmd = $config->{'ipalias'}->{'change_command'};
-        my $command = $cmd . " --dnsname=" . $dnsname . " --alias=" . $ipalias;
-        my $return_code = DBOD::Runtime->run_cmd($command);
+        my $command = $cmd . " --dnsname=" . $dnsname . " --add_ip=" . $host;
+        DEBUG 'Executing ' . $cmd;
+        return ;
+        #my $return_code = DBOD::Runtime->run_cmd($command);
+        my $return_code = 0;
         if ($return_code) {
             # An error ocurred executing external command
+            ERROR 'An error occurred creating DNS entry for ip-alias';
             return scalar 0;
         }
         else { 
+            INFO "Registerd alias %s to dnsname %s, host %s",
+                $ipalias, $dnsname, $host;
             return scalar 1;
         }
     }
     else { 
         # An error occurred getting a free dnsname. Either the DB is down
         # or there are no more dnsnames free
+        ERROR sprintf "Error registering alias %s to dnsname %s, host %s",
+            $ipalias, $dnsname, $host;
         return scalar 0;
     }
 }
@@ -55,9 +68,42 @@ sub remove_alias {
     #
     # Returns false if it fails, true if it succeeds
     
-    
-    return;
+    my ($entity, $config) = @_;
+    my $result = DBOD::Api::get_ip_alias($entity, $config);
+    DBOD::Api::remove_ip_alias($entity, $config);
+    my ($dnsname, $host, $ipalias);
+    if (defined $result) {
+        ($dnsname, $ipalias) = @{$result->{'response'}};
+        # Extract dnsname and alias from JSON result of Api call
+        # Register ip alias to dns name on the CERN Network service
+        DBOD::Network::remove_ip_alias($dnsname, $ipalias, $config);
+        # Generates DNS entry
+        my $cmd = $config->{'ipalias'}->{'change_command'};
+        my $command = $cmd . " --dnsname=" . $dnsname . " --add_ip=" . $host;
+        DEBUG 'Executing ' . $cmd;
+        return ;
+        #my $return_code = DBOD::Runtime->run_cmd($command);
+        my $return_code = 0;
+        if ($return_code) {
+            # An error ocurred executing external command
+            ERROR 'An error occurred creating DNS entry for ip-alias';
+            return scalar 0;
+        }
+        else { 
+            INFO "Registerd alias %s to dnsname %s, host %s",
+                $ipalias, $dnsname, $host;
+            return scalar 1;
+        }
+    }
+    else { 
+        # An error occurred getting a free dnsname. Either the DB is down
+        # or there are no more dnsnames free
+        ERROR sprintf "Error registering alias %s to dnsname %s, host %s",
+            $ipalias, $dnsname, $host;
+        return scalar 0;
+    }
 }
+    
 
 # TODO
 sub migrate_alias {
