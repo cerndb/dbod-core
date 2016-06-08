e4c170b8bebbb602709494535698f4081948624f
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2016-06-08 12:09:42 +0200

    REFACTOR: method rename (check_times -> is_valid)

--- lib/DBOD/Storage/NetApp/Snapshot.pm
+++ lib/DBOD/Storage/NetApp/Snapshot.pm
@@ -13,12 +13,12 @@ use Log::Log4perl qw(:easy);
 use Time::Local;
 
 # TODO: Substitute the Regexp validations for actual Date types checks.
-
-sub check_times {
+# TODO: Add version validation
+sub is_valid {
     # Check snapshot file format and PITR provided
     # Expected to return the snapshot version
     my ($snapshot, $pitr, $current_version) = @_;
-    my ($numsecs, $version) = validate($snapshot);
+    my ($numsecs, $version) = validate_snapshot($snapshot);
     my $numsecs_pitr = validate_PITR($pitr);
     if ($version != $current_version){
         ERROR "Snapshot version missmatch";
@@ -38,7 +38,7 @@ sub check_times {
     return 1;
 }
 
-sub validate {
+sub validate_snapshot {
     my $snapshot = shift;
     if ( $snapshot =~ /snapscript_(\d\d)(\d\d)(\d\d\d\d)_(\d\d)(\d\d)(\d\d)_(\d+)_(\d+)/x ) {
         my ($year, $month, $day, $hour, $min, $sec, $binlog, $version);
