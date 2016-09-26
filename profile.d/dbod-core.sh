9819d31572eb9896e2fa2e8d09c591849d58c3c9
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2016-09-26 13:14:37 +0200

    RPM: Adds SCL enabling to the profile. Fixes PERL5LIB

--- profile.d/dbod-core.sh
+++ profile.d/dbod-core.sh
@@ -1,4 +1,9 @@
 dbod_core_base=/opt/dbod/perl5
-export PERL5LIB=${PERL5LIB}:${dbod_core_base}
+# Initialize SCL for Perl520
+export PATH=/opt/rh/rh-perl520/root/usr/local/bin:/opt/rh/rh-perl520/root/usr/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/opt/rh/rh-perl520/root/usr/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export MANPATH=/opt/rh/rh-perl520/root/usr/share/man:${MANPATH}
+# Sets DBOD Core environemnt
+export PERL5LIB=${PERL5LIB}:${dbod_core_base}/lib/perl5
 export PATH=${PATH}:${dbod_core_base}/bin
 
